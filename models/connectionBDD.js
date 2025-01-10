127609b65c7625e782f39ccd64b5cd9dd5252672
RaphLho <raphael5lhommeau@gmail.com>
2025-01-10 14:20:32 +0100

    modification .env

--- models/connectionBDD.js
+++ models/connectionBDD.js
@@ -1,13 +1,23 @@
+require('dotenv').config();
+
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('TPgpuBDD', 'API_BDD', 'fox', {
-    host: 'localhost',
-    dialect: 'mssql',
-    port: 1433,
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT,
     dialectOptions: {
         encrypt: true,
         trustServerCertificate: true
     }
+});
+
+console.log('DB Config:', {
+    name: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT
 });
 
 // Tester la connexion
